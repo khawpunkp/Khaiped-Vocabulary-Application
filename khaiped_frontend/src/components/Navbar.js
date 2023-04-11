7d99f534a233fd317bdaa8fe3585518e6ff5ea2e
Krittayos Poomthong <krittayos.kp@gmail.com>
2023-04-11 21:18:17 +0700

    add | icon

--- khaiped_frontend/src/components/Navbar.js
+++ khaiped_frontend/src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react";
+import user_icon from "../assets/svg/User.svg";
+import logo from "../assets/svg/Logo.svg";
 
-function Navbar(){
-    return <header class="text-3xl">KhaiPed</header>
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div className="bg-black">
+          <img src={logo} alt="Khaiped Logo" />
+        </div>
+        <div className="searchbar "></div>        
+        <div className="user">
+          <img src={user_icon} alt="User Icon" />
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
