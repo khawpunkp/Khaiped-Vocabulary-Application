9b3e9c41784d040e8098a3ed7f7cf250caac26e4
Krittayos Poomthong <krittayos.kp@gmail.com>
2023-04-12 17:07:41 +0700

    add | font

--- khaiped_frontend/src/components/Searchbar.js
+++ khaiped_frontend/src/components/Searchbar.js
@@ -3,13 +3,13 @@ import magnify from "../assets/svg/Magnify.svg";
 
 function Searchbar() {
   return (
-    <div className="bg-white px-4 py-2 rounded-full flex justify-between items-center">
+    <div className="bg-white px-4 py-1 rounded-full flex justify-between items-center">
       <input
         type="text"
         placeholder="Search a word"
-        className="w-full bg-transparent outline-none text-lg font-bold placeholder-[#590070] placeholder-opacity-[0.28]"
+        className="w-full bg-transparent pt-2 pb-1 outline-none text-lg placeholder-[#590070] placeholder-opacity-[0.28]"
       />
-      <button className="">
+      <button className="py-2">
         <img src={magnify} alt="Magnify Icon"/>
       </button>
     </div>
