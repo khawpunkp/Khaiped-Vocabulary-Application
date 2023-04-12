f1c270b2db4c188b2fcaccbfc758ced04676f259
Krittayos Poomthong <krittayos.kp@gmail.com>
2023-04-13 00:42:13 +0700

    add | Button bg

--- khaiped_frontend/src/components/HomeButtonContainer.js
+++ khaiped_frontend/src/components/HomeButtonContainer.js
@@ -1,5 +1,9 @@
 import React from "react";
 import randomButton from "../assets/svg/ButtonRandomEmpty.svg";
+import cardButton from "../assets/svg/ButtonCardEmpty.svg";
+import gameButton from "../assets/svg/ButtonGameEmpty.svg";
+import quizButton from "../assets/svg/ButtonQuizEmpty.svg";
+import bookButton from "../assets/svg/ButtonBookEmpty.svg";
 import HomeButton from './HomeButton'
 
 function HomeButtonContainer(){
@@ -9,16 +13,16 @@ function HomeButtonContainer(){
                 <HomeButton buttonImg = {randomButton} title = "Random Word" description = "New word you have never learn"/>
             </div>
             <div className="RandomButton">
-                <HomeButton buttonImg = {randomButton} title = "Flashcard" description = "Memorise the word"/>
+                <HomeButton buttonImg = {cardButton} title = "Flashcard" description = "Memorise the word"/>
             </div>
             <div className="RandomButton">
-                <HomeButton buttonImg = {randomButton} title = "Word Scramble" description = "Have fun!"/>
+                <HomeButton buttonImg = {gameButton} title = "Word Scramble" description = "Have fun!"/>
             </div>
             <div className="RandomButton">
-                <HomeButton buttonImg = {randomButton} title = "Quiz" description = "Let’s test your knowledge"/>
+                <HomeButton buttonImg = {quizButton} title = "Quiz" description = "Let’s test your knowledge"/>
             </div>
             <div className="RandomButton">
-                <HomeButton buttonImg = {randomButton} title = "Dictionary" description = "An ordinary dictionary"/>
+                <HomeButton buttonImg = {bookButton} title = "Dictionary" description = "An ordinary dictionary"/>
             </div>
         </div>
     )
