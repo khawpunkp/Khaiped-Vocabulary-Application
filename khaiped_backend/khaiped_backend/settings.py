2d5986682d143e794b5b46dd489e061984ce4b0e
kp <krittayos.kp@gmail.com>
2023-06-05 16:40:18 +0700

    add | static file

--- khaiped_backend/khaiped_backend/settings.py
+++ khaiped_backend/khaiped_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,8 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l*pj91j@kj9tm7_xa47t6emm@aukvhy#t#9=b+$acdmvc#!z1^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
+ALLOWED_HOSTS = ['localhost', 'khawpunkp.pythonanywhere.com']
+                 
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -140,6 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
